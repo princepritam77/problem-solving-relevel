9550e77dcff85a87387b320aa76fa6a260e43381
prince pritam <108021374+princepritam77@users.noreply.github.com>
2022-07-05 23:08:11 +0530

    Add files via upload

---
+++ 2darray2.js
@@ -0,0 +1,19 @@
+let twoDarray = [
+    ["prince" ,"student", "90"],
+    ["mayank","teacher","110"],
+    ["vinay","cook","230"],
+];
+for(let i = 0; i <=twoDarray.length-1; i++) {
+console.log(twoDarray[i]);
+}
+/*
+*/
+let toDarray = [
+    [66,77,88,99],
+    [12,34,56,78,],
+    [90,87,86,84],
+];
+for(let a = 0; a <=toDarray.length-1; a++) {
+    for(let b = 0; b  <=toDarray[a].length-1; b++)
+console.log(toDarray[a][b]*5);
+}
